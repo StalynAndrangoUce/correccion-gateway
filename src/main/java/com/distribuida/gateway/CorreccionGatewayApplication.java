d09fbdb49f3bda620273e636ac00beb9db8607e5
Stalyn Andrango <andrango_stalyn@hotmail.com>
2020-02-29 13:41:18 -0500

    first commit

---
+++ src/main/java/com/distribuida/gateway/CorreccionGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.distribuida.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorreccionGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorreccionGatewayApplication.class, args);
+	}
+
+}
